917b69cd6dd31a27647f529332c2c6655373aab5
denchikj <igoshevdenis9876@gmail.com>
2023-01-17 12:49:04 +0500

    Repl

--- scheme/Program.fs
+++ scheme/Program.fs
@@ -2,17 +2,21 @@
 open NUnit.Framework
 open Parser
 open Eval
+open Repl
 
 [<Test>]
 let ``test hello`` () = 1 + 1 |> should equal 2
 
 [<EntryPoint>]
 let main argv =
-    let result =
-        argv |> Array.tryHead |> Option.defaultValue "" |> readExpr |> Result.bind eval
+    if argv.Length = 0 then
+        runRepl ()
+    else
+        let result =
+            argv |> Array.tryHead |> Option.defaultValue "" |> readExpr |> Result.bind eval
 
-    match result with
-    | Result.Ok v -> printfn "evaluated: %s" (v.ToString())
-    | Result.Error e -> printfn "error: %s" (e.ToString())
+        match result with
+        | Ok v -> printfn "%s" (v.ToString())
+        | Error e -> printfn "error: %s" (e.ToString())
 
     0
